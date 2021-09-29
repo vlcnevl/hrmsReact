5cbad6a5695969985af2ab9dcc921309b2a9989a
Velican Evli <veli-can-evli06@hotmail.com>
2021-09-29 19:03:12 +0300

    .

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -70,7 +70,7 @@ export default function Register() {
                   {...register("email", {
                     required: true,
                     pattern: /\S+@\S+\.\S+/,
-                  })}
+                  })} 
                   name="email"
                   className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 />
